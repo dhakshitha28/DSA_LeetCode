3e43ba574f3f1d44e89fc81b6a595415546a978e
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-17 20:11:47 +0530

    Left Rotate Array by one

---
+++ Rotate_Array_by_One/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void rotate(int[] arr) {
+        int n = arr.length;
+        int[] temp = new int[n];
+
+        temp[0] = arr[n - 1];
+        for (int i = 0; i < n - 1; i++) {
+            temp[i + 1] = arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = temp[i];
+        }
+    }
+}
