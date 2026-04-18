0c2760ea035cd0197e70a8f745ed3807c0f91817
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-18 10:46:35 +0530

    Isomorphic string

---
+++ Isomorphic_Strings/Solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        boolean result=false;
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        if(s.equals(t)){
+            return true;
+        }
+
+        Map<Character,Character> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            if (!map.containsKey(s.charAt(i))) {
+
+                if (map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+
+                map.put(s.charAt(i),t.charAt(i));
+                result =true;
+            }
+            else if(map.containsKey(s.charAt(i))){
+                if(t.charAt(i)==map.get(s.charAt(i))){
+                    result =true;
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return result;
+
+    }
+}
