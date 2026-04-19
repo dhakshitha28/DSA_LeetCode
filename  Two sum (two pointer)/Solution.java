e83384d9141e75baa5393765b4f0483d4e316c9d
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-19 23:35:23 +0530

    Two sum solved using two pointer

---
+++  Two sum (two pointer)/Solution.java
@@ -0,0 +1,23 @@
+public class TwoSum {
+    public int[] twoSum(int[] numbers, int target) {
+
+        int[] result = new int[2];
+        int i = 0;
+        int j = numbers.length - 1;
+
+        while (i != j) {
+            if (numbers[i] + numbers[j] > target) {
+                j--;
+            } else if (numbers[i] + numbers[j] < target) {
+                i++;
+            } else {
+                result[0] = i+1;
+                result[1] = j+1;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
+
