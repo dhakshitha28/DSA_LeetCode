0e328a6657ed0955475f931897d6e3d848dfce33
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-19 17:49:00 +0530

    Roman to Integer

---
+++ Roman_to_Integer/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToInt(String s) {
+
+     Map<Character,Integer> map=new HashMap<>();
+     int result=0;
+     map.put('I',1);
+     map.put('V',5);
+     map.put('X',10);
+     map.put('L',50);
+     map.put('C',100);
+     map.put('D',500);
+     map.put('M',1000);
+     
+     int ans=0;
+        for(int i=s.length()-1;i>=0;i--){
+            int currentValue=map.get(s.charAt(i));
+           
+           if( currentValue < ans){
+                    result-=currentValue;
+           }
+           else{
+               
+               result+=currentValue;
+           }
+
+            ans=currentValue;
+        }
+     
+    return result;
+}
+}
