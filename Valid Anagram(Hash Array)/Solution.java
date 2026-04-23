19de339ae8ae3f1c05e6d028a45732bb38fc0ec2
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-04-23 22:02:44 +0530

    Valid Anagram(Hash Array)

---
+++ Valid Anagram(Hash Array)/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        // Using HashMap
+        if(s.length()!=t.length()){
+            return false;
+        }
+
+        HashMap<Character,Integer> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(map.get(s.charAt(i))!=0){
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+}
