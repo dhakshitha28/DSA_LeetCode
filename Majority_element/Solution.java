7d7b3ae32626ac98e94ac3efde3783cdce5cdb5a
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-05-09 00:03:41 +0530

    Changes in Majority element

--- Majority_element/Solution.java
+++ Majority_element/Solution.java
@@ -2,26 +2,17 @@ class Solution {
     public int majorityElement(int[] nums) {
 
 
-        List<Integer> value=new ArrayList<>();
         Map<Integer,Integer> map=new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-            if(! value.contains(nums[i])){
-                value.add(nums[i]);
+
+            if(map.get(nums[i]) > nums.length/2){
+                return nums[i];
             }
         }
 
-        int result=0;
-        int max=0;
-        for(int i=0;i<value.size();i++){
-            if(map.get(value.get(i))>max){
-                result=value.get(i);
-                max=map.get(nums[i]);
-            }
-        }
-
-        return result;
+        return -1;
 
     }
 }
