cfc6cc50b2dec9315bacac42ea17df983740d9ae
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-06-07 22:56:10 +0530

    Defuse the Bomb

---
+++ defuse the bomb/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] decrypt(int[] code, int k) {
+
+        int [] result=new int[code.length];
+        int n=code.length;
+
+        if(k==0){
+            return result;
+        }
+
+        int start=1,end=k,sum=0;
+        if(k<0){
+            start=n-Math.abs(k);
+            end=n-1;
+        }
+        for(int i=start;i<=end;i++){
+            sum+=code[i];
+        }
+        for(int i=0;i<n;i++){
+            result[i]=sum;
+            sum-=code[start%n];
+            sum+=code[(end+1)%n];
+            start++;
+            end++;
+        }
+
+        return result;
+    }
+}
