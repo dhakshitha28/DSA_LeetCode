8e5d78f14c77b5153fe98a5b0a6eef3127f3097c
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-06-15 21:48:51 +0530

    Sort Array By Parity

---
+++  Sort Array By Parity/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+
+        int [] arr=new int [nums.length];
+        int left=0;
+        int right=nums.length-1;
+
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]%2==0){
+                arr[left]=nums[i];
+                left++;
+            }
+            else{
+                arr[right]=nums[i];
+                right--;
+            }
+        }
+
+        return arr;
+    }
+}
