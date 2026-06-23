c87e6dcd26f41c6c7fd944074c81229e295053fa
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-06-23 22:37:47 +0530

    Word pattern

---
+++ Word Pattern/Solution.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+
+        Map<Character, String> map1 = new HashMap<>();
+        Map<String, Character> map2 = new HashMap<>();
+
+        String[] words = s.split(" ");
+
+        if(pattern.length() != words.length){
+            return false;
+        }
+
+        for(int i = 0; i < words.length; i++){
+
+            char ch = pattern.charAt(i);
+            String word = words[i];
+
+
+            if(map1.containsKey(ch)){
+
+                if(!map1.get(ch).equals(word)){
+                    return false;
+                }
+
+            }
+
+            else if(map2.containsKey(word)){
+                return false;
+            }
+
+            else{
+                map1.put(ch, word);
+                map2.put(word, ch);
+            }
+        }
+
+        return true;
+    }
+}
