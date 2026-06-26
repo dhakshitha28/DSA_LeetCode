088b04585be847c498631e590e32ad6d7d74f0d9
dhakshitha <dhakshitha.ac.10@gmail.com>
2026-06-26 23:20:54 +0530

    Longest Palindrome

---
+++ longest palindrome/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestPalindrome(String s) {
+
+        int[] freq = new int[128];
+
+        for(char c : s.toCharArray()){
+            freq[c]++;
+        }
+
+        int length = 0;
+        boolean odd = false;
+        for(int count : freq){
+
+            if(count % 2 == 0){
+                length += count;
+            }
+            else{
+                length += count - 1;
+                odd = true;
+            }
+        }
+
+        if(odd){
+            length += 1;
+        }
+
+        return length;
+    }
+}
